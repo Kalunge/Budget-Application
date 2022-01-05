034a967bd216498704b39d37f279076d78ab00a9
kalunge <muthomititus+github@gmail.com>
2022-01-05 09:47:38 +0100

    Update route to got to splash page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
 
-  before_action :authenticate_user!
   protect_from_forgery prepend: true
   before_action :update_allowed_parameters, if: :devise_controller?
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do
-    authenticated :user do
-      root to: 'groups#index', as: :authenticated_root
-    end
-    unauthenticated do
-      root 'splash#index', as: :unauthenticated_root
-    end
+  authenticated :user do
+    root to: 'groups#index', as: :authenticated_root
   end
-  
+
+  root to: "splash#index"
+
+  devise_for :users
   resources :groups
   resources :entities
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
