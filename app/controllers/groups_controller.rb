d20db39c48525a0ce216fdf1376c126ec89a9113
kalunge <muthomititus+github@gmail.com>
2022-01-05 11:39:35 +0100

    Add Ability to create a Category

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,31 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @groups = current_user.groups
+  end
+
+  def show
+    @group = current_user.groups.find(params[:id]) 
+    @entities = @group.entities
   end
 
   def new
+    @group = current_user.groups.new
   end
 
   def create
+    @group = current_user.groups.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to root_path, notice: 'group was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_123750) do
+ActiveRecord::Schema.define(version: 2022_01_05_100739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2022_01_04_123750) do
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -35,6 +37,8 @@ ActiveRecord::Schema.define(version: 2022_01_04_123750) do
     t.string "icon"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +54,6 @@ ActiveRecord::Schema.define(version: 2022_01_04_123750) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "users"
+  add_foreign_key "groups", "users"
 end
